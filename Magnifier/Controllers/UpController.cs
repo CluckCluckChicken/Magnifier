eff2131f003a7c9df0060f89603e423d9316e2c5
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2021-06-19 22:35:53 +0100

    Create UpController.cs

---
+++ Magnifier/Controllers/UpController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Magnifier.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UpController : ControllerBase
+    {
+        [HttpGet("up")]
+        public ActionResult GetUp()
+        {
+            return NoContent();
+        }
+    }
+}
