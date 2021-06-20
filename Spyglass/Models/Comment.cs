81d7d15e106b951391d4974f98b15f57b80bf2e5
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2021-06-20 07:52:00 +0100

    store residence and residenceId of comments

--- Spyglass/Models/Comment.cs
+++ Spyglass/Models/Comment.cs
@@ -5,14 +5,23 @@ using System.Threading.Tasks;
 
 namespace Magnifier.Models
 {
+    public enum Residence
+    {
+        Project,
+        User,
+        Studio
+    }
+
     public record Comment
     {
         public Comment() { }
 
-        public Comment(int commentId, ScratchComment comment, bool isReply, List<Magnifier.Models.Comment> replies)
+        public Comment(int commentId, ScratchComment comment, Residence residence, string residenceId, bool isReply, List<Comment> replies)
         {
             this.commentId = commentId;
             this.comment = comment;
+            this.residence = residence;
+            this.residenceId = residenceId;
             this.isReply = isReply;
             this.replies = replies;
 
@@ -25,12 +34,16 @@ namespace Magnifier.Models
 
         public ScratchComment comment { get; set; }
 
+        public Residence residence { get; set; }
+
+        public string residenceId { get; set; }
+
         public List<UserReaction> reactions { get; set; }
 
         public bool isPinned { get; set; }
 
         public bool isReply { get; set; }
 
-        public List<Magnifier.Models.Comment> replies { get; set; }
+        public List<Comment> replies { get; set; }
     }
 }
