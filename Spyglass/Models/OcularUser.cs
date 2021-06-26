ce509207f5b0a8703d2867a0afc602e6c49a77b9
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2021-06-26 18:06:15 +0100

    profile pages revamp part 1

---
+++ Spyglass/Models/OcularUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Spyglass.Models
+{
+    public class OcularUser
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+
+        [JsonPropertyName("color")]
+        public string Color { get; set; }
+    }
+}
---
+++ Spyglass/Models/ScratchUser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Spyglass.Models
+{
+    public record ScratchUser
+    {
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("sys_id")]
+        public int SysId { get; set; }
+
+        [JsonPropertyName("joined")]
+        public DateTime Joined { get; set; }
+
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+
+        [JsonPropertyName("bio")]
+        public string Bio { get; set; }
+
+        [JsonPropertyName("work")]
+        public string Work { get; set; }
+
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+
+        [JsonPropertyName("school")]
+        public object School { get; set; }
+    }
+}
